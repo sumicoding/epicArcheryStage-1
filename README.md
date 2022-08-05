2c2674c443f055a6d309a3645af9e0c89a18e873
sumicoding <104206170+sumicoding@users.noreply.github.com>
2022-08-05 19:05:00 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# epicArcheryStage-1
+# Project 23
+
+Project Solution 23
---
+++ js/Player.js
@@ -0,0 +1,33 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+    this.width = 40
+    this.height = 80
+//create the rectangular shaped body.
+this.body=Bodies.rectangle(x,y,this.width,this.height,options);
+   
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+    
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png")
+    World.add(world, this.body);
+  }
+
+ 
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
